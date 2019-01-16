27cfb21e83b61129db381b6baf3f627694087711
bullsean <41093836+bullsean@users.noreply.github.com>
2019-01-15 21:41:23 -0600

    Initial Look and Feel setup

---
+++ assets/javascript/main.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyAuqWjsh4QNYw457DPjvaIsanDdNt7-8gU",
+    authDomain: "trainschedulersb.firebaseapp.com",
+    databaseURL: "https://trainschedulersb.firebaseio.com",
+    projectId: "trainschedulersb",
+    storageBucket: "",
+    messagingSenderId: "717591959352"
+  };
+  firebase.initializeApp(config);
