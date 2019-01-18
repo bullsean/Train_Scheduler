52f580a6b1cf96b3bcb6f8df53562d5daa717ecc
bullsean <41093836+bullsean@users.noreply.github.com>
2019-01-17 21:00:21 -0600

    Just committing to make sure

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,9 +1,9 @@
-var config = {
-    apiKey: "AIzaSyAuqWjsh4QNYw457DPjvaIsanDdNt7-8gU",
-    authDomain: "trainschedulersb.firebaseapp.com",
-    databaseURL: "https://trainschedulersb.firebaseio.com",
-    projectId: "trainschedulersb",
-    storageBucket: "",
-    messagingSenderId: "717591959352"
-  };
-  firebase.initializeApp(config);
+// var config = {
+//     apiKey: "AIzaSyAuqWjsh4QNYw457DPjvaIsanDdNt7-8gU",
+//     authDomain: "trainschedulersb.firebaseapp.com",
+//     databaseURL: "https://trainschedulersb.firebaseio.com",
+//     projectId: "trainschedulersb",
+//     storageBucket: "",
+//     messagingSenderId: "717591959352"
+//   };
+//   firebase.initializeApp(config);
